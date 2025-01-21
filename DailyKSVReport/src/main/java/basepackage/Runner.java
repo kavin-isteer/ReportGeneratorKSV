ffba53b8f7d3a23c5096e9f3c6dcc7cd22f1518f
kavin-isteer <kavinkr.isteer.com>
2025-01-21 18:28:12 +0530

    deleted source code and kept only packaged files

--- DailyKSVReport/src/main/java/basepackage/Runner.java
+++
@@ -1,9 +0,0 @@
-package basepackage;
-
-public class Runner {
-	public static void main(String[] args) {
-		System.out.println("Application started!!");
-		ReportService service = new ReportService();
-		service.fetchData();
-	}
-}
